2d5143a0dcd74701b170d853b84e88f4b53ce76b
RacoonWally <eeewwwrrr@mail.ru>
2020-08-18 22:22:13 +0300

    first commit

---
+++ src/pages/mainPage/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class MainPage extends Component {
+    render() {
+        return(
+            <div>
+                MainPage
+            </div>
+        )
+    }
+}
+export default MainPage;
