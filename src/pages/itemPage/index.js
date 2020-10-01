042c3f0ba95511049e249aa1241c176af21170cc
RacoonWally <eeewwwrrr@mail.ru>
2020-10-02 01:23:30 +0300

    first commit

---
+++ src/pages/itemPage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './index.scss'
+
+const Item = () => {
+    return (
+        <div>
+            Item
+        </div>
+    )
+};
+
+export default Item;
---
+++ src/сomponents/header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './index.scss'
+
+const NavBar = () => {
+    return (
+        <div>
+            navBar
+        </div>
+    )
+};
+
+export default NavBar;
